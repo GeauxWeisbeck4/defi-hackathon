2baf93f0d716159be04cba889534dc4b465a1341
Andrew <andrew.weisbeck@gmail.com>
2022-07-31 19:35:37 -0400

    Please enter the commit message for your changes. Lines starting

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,67 +1,62 @@
 import { twitterIcon, youtubeIcon, githubIcon, instagramIcon } from "../icons/icons";
 
 
-export const footerSocials = [
-  {
-    url: 'https://instagram.com/',
-    icon: instagramIcon,
-  },
-  {
-    url: 'https://youtube.com/',
-    icon: youtubeIcon,
-  },
-  {
-    url: 'https://twitter.com/littlesticksdev',
-    icon: twitterIcon,
-  },
-  {
-    url: 'https://github.com/littlesticksdev/odyssey-theme',
-    icon: githubIcon,
-  },
+export const footerSocials = [{
+        url: 'https://instagram.com/',
+        icon: instagramIcon,
+    },
+    {
+        url: 'https://youtube.com/',
+        icon: youtubeIcon,
+    },
+    {
+        url: 'https://twitter.com/littlesticksdev',
+        icon: twitterIcon,
+    },
+    {
+        url: 'https://github.com/littlesticksdev/odyssey-theme',
+        icon: githubIcon,
+    },
 ]
 
-export const footerLists = [
-	{
-		title: 'Upcoming Events',
-		items: [
-			{
-				title: 'Event 1',
-				slug: '/upcoming-events/info/event-1',
-			},
-			{
-				title: 'Event 2',
-				slug: '/upcoming-events/info/event-2',
-			},
-		],
-	},
-	{
-		title: 'Company',
-		items: [
-			{
-				title: 'About',
-				slug: '/company/about',
-			},
-			{
-				title: 'News',
-				slug: '/news',
-			},
-			{
-				title: 'Contact',
-				slug: '/company/contact',
-			},
-		],
-	},
-	{
-		title: 'Resources',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-		],
-	},
+export const footerLists = [{
+        title: 'Upcoming Events',
+        items: [{
+                title: 'First Even',
+                slug: '/upcoming-events/info/event-1',
+            },
+            {
+                title: 'Event 2',
+                slug: '/upcoming-events/info/event-2',
+            },
+        ],
+    },
+    {
+        title: 'Company',
+        items: [{
+                title: 'About',
+                slug: '/company/about',
+            },
+            {
+                title: 'News',
+                slug: '/news',
+            },
+            {
+                title: 'Contact',
+                slug: '/company/contact',
+            },
+        ],
+    },
+    {
+        title: 'Resources',
+        items: [{
+                title: 'Get Started',
+                slug: '/theme/get-started',
+            },
+            {
+                title: 'Style Guide',
+                slug: '/theme/style-guide',
+            },
+        ],
+    },
 ];
